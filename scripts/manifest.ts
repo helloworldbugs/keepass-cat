9fcad2238850410bf09879168759f02b3f3c8e83
helloworldbugs <alive13460@gmail.com>
2026-09-09 01:49:15 +0800

    update

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -34,10 +34,10 @@ const permissions = [
 ];
 
 const baseManifest: Manifest.WebExtensionManifest = {
-  name: 'Keepass Cat - WebDav',
+  name: 'Keepass Cat - WebDAV',
   short_name: 'Keepass Cat',
   version: '1.0.0',
-  description: 'Open-source KeePass browser extension: connect your WebDAV database for one-click autofill, TOTP 2FA, and editing — self-hosted and secure.',
+  description: 'Open-source KeePass extension: connect your WebDAV database for one-click autofill, TOTP 2FA, and editing — self-hosted and secure.',
   default_locale: 'en',
   icons: {
     '16': '/assets/16x16.png',
